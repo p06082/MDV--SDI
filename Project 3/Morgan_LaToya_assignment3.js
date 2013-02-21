6318129b72333ffad57cd0759500c233257661e1
LaToya Morgan <latoya.jeanette@gmail.com>
2013-02-21 17:52:52 -0500

    variables, array with for loop

--- Project 3/Morgan_LaToya_assignment3.js
+++ Project 3/Morgan_LaToya_assignment3.js
@@ -1,1 +1,30 @@
-alert("JavaScript works!");
+alert("JavaScript works!");//alert("JavaScript works!");
+
+/*  LaToya Morgan
+	Feb 6, 2013
+	Project 3
+	Shaman aiding allies in an epic battle
+*/
+
+// Global Variables
+	var myClass = "shaman"
+	var enemyName = "\"Drackensore\""
+	var taskName = "healing"
+	var weapon1 = "staff"
+	var weapon2 = "mace and shield"
+	var enemyCount = 10;
+	var powerStrain = true;
+	
+	
+	
+console.log("" +enemyName+ " has just made a powerful attack against us all!");
+	
+	
+		var allyNames = ["Aline","Rayne","Poppy","Zeke"],
+			secondsToHeal = 5;
+	
+		for (var i = 0; i < allyNames.length; i++) {
+			console.log("I have " +secondsToHeal+ " seconds to heal " +allyNames[0]+ "!");
+		}
+	
+		console.log("I have succesfully healed everyone's wounds!");
