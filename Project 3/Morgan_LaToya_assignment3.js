0480723fd5d5a94054d04c1e3d462db5ded02abf
LaToya Morgan <latoya.jeanette@gmail.com>
2013-02-21 20:07:10 -0500

    added conditionals, moved enemy console.log

--- Project 3/Morgan_LaToya_assignment3.js
+++ Project 3/Morgan_LaToya_assignment3.js
@@ -1,4 +1,4 @@
-//alert("JavaScript works!");
+// alert("JavaScript works!");
 
 /*  LaToya Morgan
 	Feb 6, 2013
@@ -15,12 +15,39 @@
 	var enemyCount = 10;
 	var powerStrain = true;
 	
-	
-	
+
+
+// Conditionals
+
 console.log("" +enemyName+ " has just made a powerful attack against us all!");
+
+if (powerStrain === true) {
+	if (enemyCount <= 10) {
+		enemyCount = enemyCount + 15;
+		console.log ("With a sudden invasion of more enemies, bringing the total to " + enemyCount + ", I have come to the conclusion that a sacrifice must be made, and that we must retreat before all is lost.")
+		
+	} else {
+		console.log ("We will win this battle!")
+	}
 	
+} else {
+	if (powerStrain === false) {
+		if (enemyCount <= 10) {
+			enemyCount = 1;
+			console.log ("We have successfully defeated our foe's accomplices, leaving only " + enemyCount + ", the infamous " + enemyName + ".")
+		}
+	} else {
+		console.log ("We may want to call for reinforcements.")
+	}
+
+}
+
+
+
+// Array Function
+
 	function alliesHurt ()
-	
+
 	{	var allyNames = ["Aline","Rayne","Poppy","Zeke"],
 			healTime = 5;
 
@@ -28,5 +55,7 @@ console.log("" +enemyName+ " has just made a powerful attack against us all!");
 			console.log("I have " +healTime+ " seconds to heal " +allyNames[i]+ "!");
 
 	}
+
+}
 
 alliesHurt();
